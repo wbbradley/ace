522e0034a897d941ff24c917558f27b55fadb933
William Bradley <williambbradley@gmail.com>
2017-09-12 00:07:47 +0000

    get more info from tests

--- make-and-run-tests.sh
+++ make-and-run-tests.sh
@@ -3,14 +3,11 @@ set +e
 set +x
 
 cd /opt/zion
-ls -tr -la | grep -i cmake
 cmake --version
 ctest --version
 
 cmake .
-ls -tr -la | grep -i cmake
+make -j8
 
-make -j4
-ls -tr -la | grep -i cmake
-
-ctest
+./zionc test
+./zionc run test_hello_world
