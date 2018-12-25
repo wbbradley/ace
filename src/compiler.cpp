0e0e9f26e03f5dbf7da36b2e56eca5c6975a17b7
William Bradley <williambbradley@gmail.com>
2018-12-25 10:20:10 -0600

    get rid of pointlessly mutable and stateful compiler_t

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -43,262 +43,210 @@ const std::vector<std::string> &get_zion_paths() {
 	return zion_paths;
 }
 
-compiler_t::compiler_t(std::string program_name_) :
-	program_name(strip_zion_extension(program_name_))
-{
-}
+namespace compiler {
 
-compiler_t::~compiler_t() {
-}
-
-void compiler_t::info(const char *format, ...) {
-	va_list args;
-	va_start(args, format);
-	logv(log_info, format, args);
-	va_end(args);
-}
-
-std::string compiler_t::get_executable_filename() const {
-	return "z.out";
-}
-
-std::string resolve_module_filename(
-		location_t location,
-		std::string name,
-		std::string extension)
-{
-	std::string filename_test_resolution;
-	if (real_path(name, filename_test_resolution)) {
-		if (name == filename_test_resolution) {
-			if (!ends_with(filename_test_resolution, extension)) {
-				filename_test_resolution = filename_test_resolution + extension;
-			}
-
-			if (file_exists(filename_test_resolution)) {
-				/* short circuit if we're given a real path */
-				return filename_test_resolution;
-			} else {
-				panic(string_format("filename %s does not exist", name.c_str()));
-			}
-		} else if (extension == "" && file_exists(filename_test_resolution)) {
-			return filename_test_resolution;
-		}
+	std::string get_executable_filename() {
+		return "z.out";
 	}
 
-	std::string leaf_name = name + extension;
-	std::string working_resolution;
-	for (auto zion_path : get_zion_paths()) {
-		auto test_path = zion_path + "/" + leaf_name;
-		if (file_exists(test_path)) {
-			std::string test_resolution;
-			if (real_path(test_path, test_resolution)) {
-				if (working_resolution.size() && working_resolution != test_resolution) {
-					throw user_error(location, "multiple " C_FILENAME "%s"
-							C_RESET " modules found with the same name in source "
-							"path [%s, %s]", name.c_str(),
-							working_resolution.c_str(),
-							test_resolution.c_str());
+	std::string resolve_module_filename(
+			location_t location,
+			std::string name,
+			std::string extension)
+	{
+		std::string filename_test_resolution;
+		if (real_path(name, filename_test_resolution)) {
+			if (name == filename_test_resolution) {
+				if (!ends_with(filename_test_resolution, extension)) {
+					filename_test_resolution = filename_test_resolution + extension;
+				}
+
+				if (file_exists(filename_test_resolution)) {
+					/* short circuit if we're given a real path */
+					return filename_test_resolution;
 				} else {
-					working_resolution = test_resolution;
-					debug_above(11, log(log_info, "searching for file %s, found it at %s",
+					panic(string_format("filename %s does not exist", name.c_str()));
+				}
+			} else if (extension == "" && file_exists(filename_test_resolution)) {
+				return filename_test_resolution;
+			}
+		}
+
+		std::string leaf_name = name + extension;
+		std::string working_resolution;
+		for (auto zion_path : get_zion_paths()) {
+			auto test_path = zion_path + "/" + leaf_name;
+			if (file_exists(test_path)) {
+				std::string test_resolution;
+				if (real_path(test_path, test_resolution)) {
+					if (working_resolution.size() && working_resolution != test_resolution) {
+						throw user_error(location, "multiple " C_FILENAME "%s"
+								C_RESET " modules found with the same name in source "
+								"path [%s, %s]", name.c_str(),
+								working_resolution.c_str(),
+								test_resolution.c_str());
+					} else {
+						working_resolution = test_resolution;
+						debug_above(11, log(log_info, "searching for file %s, found it at %s",
+									name.c_str(),
+									working_resolution.c_str()));
+					}
+				} else {
+					/* if the file exists, it should have a real_path */
+					panic(string_format("searching for file %s, unable to resolve its real path (%s)",
 								name.c_str(),
-								working_resolution.c_str()));
+								test_path.c_str()));
 				}
 			} else {
-				/* if the file exists, it should have a real_path */
-				panic(string_format("searching for file %s, unable to resolve its real path (%s)",
+				debug_above(11, log(log_info, "searching for file %s, did not find it at %s",
 							name.c_str(),
 							test_path.c_str()));
 			}
+		}
+
+		if (working_resolution.size() != 0) {
+			/* cool, we found one and only one module with the requested name in
+			 * the source paths */
+			return working_resolution;
 		} else {
-			debug_above(11, log(log_info, "searching for file %s, did not find it at %s",
-						name.c_str(),
-						test_path.c_str()));
+			throw user_error(location, "module not found: " c_error("`%s`") " (Note that module names should not have .zion extensions.) Looked in ZION_PATH=[%s]",
+					name.c_str(),
+					join(get_zion_paths(), ":").c_str());
+			return "";
 		}
 	}
 
-	if (working_resolution.size() != 0) {
-		/* cool, we found one and only one module with the requested name in
-		 * the source paths */
-		return working_resolution;
-	} else {
-		throw user_error(location, "module not found: " c_error("`%s`") " (Note that module names should not have .zion extensions.) Looked in ZION_PATH=[%s]",
-				name.c_str(),
-				join(get_zion_paths(), ":").c_str());
-		return "";
-	}
-}
+	struct global_parser_state_t {
 
-struct global_parser_state_t {
+		std::vector<module_t *> modules;
+		std::map<std::string, module_t *> modules_map_by_filename;
+		std::map<std::string, module_t *> modules_map_by_name;
+		std::vector<token_t> comments;
+		std::set<token_t> link_ins;
 
-	std::vector<module_t *> modules;
-	std::map<std::string, module_t *> modules_map_by_filename;
-	std::map<std::string, module_t *> modules_map_by_name;
-	std::vector<token_t> comments;
-	std::set<token_t> link_ins;
-
-	module_t *parse_module(identifier_t module_id) {
-		if (auto module = get(modules_map_by_name, module_id.name, (module_t *)nullptr)) {
-			return module;
-		}
-		std::string module_filename = resolve_module_filename(module_id.location, module_id.name, ".zion");
-		if (auto module = get(modules_map_by_filename, module_filename, (module_t *)nullptr)) {
-			return module;
-		}
-
-		/* we found an unparsed file */
-		std::ifstream ifs;
-		ifs.open(module_filename.c_str());
-
-		if (ifs.good()) {
-			debug_above(11, log(log_info, "parsing module " c_id("%s"), module_filename.c_str()));
-			zion_lexer_t lexer({module_filename}, ifs);
-
-			parse_state_t ps(module_filename, "", lexer, &comments, &link_ins);
-
-			identifiers_t dependencies;
-			module_t *module = ::parse_module(ps, dependencies);
-
-			modules.push_back(module);
-			modules_map_by_name[ps.module_name] = module;
-			modules_map_by_filename[ps.filename] = module;
-
-			for (auto dependency : dependencies) {
-				parse_module(dependency);
+		module_t *parse_module(identifier_t module_id) {
+			if (auto module = get(modules_map_by_name, module_id.name, (module_t *)nullptr)) {
+				return module;
+			}
+			std::string module_filename = compiler::resolve_module_filename(module_id.location, module_id.name, ".zion");
+			if (auto module = get(modules_map_by_filename, module_filename, (module_t *)nullptr)) {
+				return module;
 			}
 
-			return module;
-		} else {
-			auto error = user_error(module_id.location, "could not open \"%s\" when trying to link module", module_filename.c_str());
-			error.add_info(module_id.location, "imported here");
-			throw error;
-		}
-	}
-};
+			/* we found an unparsed file */
+			std::ifstream ifs;
+			ifs.open(module_filename.c_str());
 
+			if (ifs.good()) {
+				debug_above(11, log(log_info, "parsing module " c_id("%s"), module_filename.c_str()));
+				zion_lexer_t lexer({module_filename}, ifs);
 
-std::set<std::string> get_top_level_decls(
-		const std::vector<decl_t *> &decls,
-		const std::vector<type_decl_t> &type_decls,
-		const std::vector<type_class_t *> &type_classes)
-{
-	std::map<std::string, location_t> module_decls;
-	for (decl_t *decl : decls) {
-		if (module_decls.find(decl->var.name) != module_decls.end()) {
-			auto error = user_error(decl->var.location, "duplicate symbol");
-			error.add_info(module_decls[decl->var.name], "see prior definition here");
-			throw error;
-		}
-		module_decls[decl->var.name] = decl->var.location;
-	}
-	std::set<std::string> top_level_decls;
-	for (auto pair : module_decls) {
-		top_level_decls.insert(pair.first);
-	}
-	for (auto type_decl : type_decls) {
-		top_level_decls.insert(type_decl.id.name);
-	}
-	for (auto type_class : type_classes) {
-		top_level_decls.insert(type_class->id.name);
-	}
-	return top_level_decls;
-}
+				parse_state_t ps(module_filename, "", lexer, &comments, &link_ins);
 
-bool compiler_t::parse_program() {
-	try {
-		/* first just parse all the modules that are reachable from the initial module
-		 * and bring them into our whole ast */
-		auto module_name = program_name;
+				identifiers_t dependencies;
+				module_t *module = ::parse_module(ps, dependencies);
 
-		assert(program == nullptr);
+				modules.push_back(module);
+				modules_map_by_name[ps.module_name] = module;
+				modules_map_by_filename[ps.filename] = module;
 
-		global_parser_state_t gps;
+				for (auto dependency : dependencies) {
+					parse_module(dependency);
+				}
 
-		/* always include the builtins library */
-		if (getenv("NO_BUILTINS") == nullptr) {
-			gps.parse_module({"lib/builtins", location_t{"builtins", 0, 0}});
-		}
-
-		/* always include the standard library */
-		if (getenv("NO_STD_LIB") == nullptr) {
-			gps.parse_module({"lib/std", location_t{std::string(GLOBAL_SCOPE_NAME) + " lib", 0, 0}});
-		}
-
-		/* now parse the main program module */
-		gps.parse_module({module_name, location_t{"command line build parameters", 0, 0}});
-
-		debug_above(11, log(log_info, "parse_module of %s succeeded", module_name.c_str(),
-					false /*global*/));
-
-		std::vector<decl_t *> program_decls;
-		std::vector<type_class_t *> program_type_classes;
-		/* next, merge the entire set of modules into one program */
-
-		for (module_t *module : gps.modules) {
-			/* get a list of all top-level decls */
-			std::set<std::string> bindings = get_top_level_decls(module->decls, module->type_decls, module->type_classes);
-			module_t *module_rebound = prefix(bindings, module);
-
-			/* now all locally referring vars are fully qualified */
-			for (decl_t *decl : module_rebound->decls) {
-				program_decls.push_back(decl);
-			}
-			for (type_class_t *type_class : module_rebound->type_classes) {
-				program_type_classes.push_back(type_class);
+				return module;
+			} else {
+				auto error = user_error(module_id.location, "could not open \"%s\" when trying to link module", module_filename.c_str());
+				error.add_info(module_id.location, "imported here");
+				throw error;
 			}
 		}
-		assert(program == nullptr);
-		program = new program_t(
-				program_decls,
-				program_type_classes,
-			   	new application_t(
-					new var_t(make_iid("main")),
-				   	new var_t(make_iid("unit"))));
-
-		comments = gps.comments;
-		link_ins = gps.link_ins;
-
-		return true;
-	} catch (user_error &e) {
-		print_exception(e);
-		return false;
-	}
-}
+	};
 
 
-bool compiler_t::build_type_check_and_code_gen() {
-	try {
-		debug_above(2, log(log_info, "type checking found no errors"));
-		// TODO:...
-		assert(false);
-		return true;
-
-	} catch (user_error &e) {
-		print_exception(e);
-		return false;
-	}
-}
-
-std::string compute_module_key(std::vector<std::string> lib_paths, std::string filename) {
-	std::string working_key;
-	for (auto lib_path : lib_paths) {
-		if (starts_with(filename, lib_path)) {
-			if (working_key.size() < (filename.size() - lib_path.size())) {
-				working_key = filename.substr(lib_path.size() + 1);
-				assert(ends_with(working_key, ".zion"));
-				working_key = working_key.substr(0, working_key.size() - strlen(".zion"));
+	std::set<std::string> get_top_level_decls(
+			const std::vector<decl_t *> &decls,
+			const std::vector<type_decl_t> &type_decls,
+			const std::vector<type_class_t *> &type_classes)
+	{
+		std::map<std::string, location_t> module_decls;
+		for (decl_t *decl : decls) {
+			if (module_decls.find(decl->var.name) != module_decls.end()) {
+				auto error = user_error(decl->var.location, "duplicate symbol");
+				error.add_info(module_decls[decl->var.name], "see prior definition here");
+				throw error;
 			}
+			module_decls[decl->var.name] = decl->var.location;
 		}
+		std::set<std::string> top_level_decls;
+		for (auto pair : module_decls) {
+			top_level_decls.insert(pair.first);
+		}
+		for (auto type_decl : type_decls) {
+			top_level_decls.insert(type_decl.id.name);
+		}
+		for (auto type_class : type_classes) {
+			top_level_decls.insert(type_class->id.name);
+		}
+		return top_level_decls;
+	}
 
-	}
-	if (working_key.size() == 0) {
-		if (filename[0] == '/') {
-				assert(ends_with(filename, ".zion"));
-				working_key = filename.substr(0, filename.size() - strlen(".zion"));
-		} else {
-			panic(string_format("could not find module filename in lib paths (%s)",
-						filename.c_str()));
+	compilation_t::ref parse_program(std::string user_program_name) {
+		std::string program_name = strip_zion_extension(user_program_name);
+		try {
+			/* first just parse all the modules that are reachable from the initial module
+			 * and bring them into our whole ast */
+			auto module_name = program_name;
+
+			global_parser_state_t gps;
+
+			/* always include the builtins library */
+			if (getenv("NO_BUILTINS") == nullptr) {
+				gps.parse_module({"lib/builtins", location_t{"builtins", 0, 0}});
+			}
+
+			/* always include the standard library */
+			if (getenv("NO_STD_LIB") == nullptr) {
+				gps.parse_module({"lib/std", location_t{std::string(GLOBAL_SCOPE_NAME) + " lib", 0, 0}});
+			}
+
+			/* now parse the main program module */
+			gps.parse_module({module_name, location_t{"command line build parameters", 0, 0}});
+
+			debug_above(11, log(log_info, "parse_module of %s succeeded", module_name.c_str(),
+						false /*global*/));
+
+			std::vector<decl_t *> program_decls;
+			std::vector<type_class_t *> program_type_classes;
+			/* next, merge the entire set of modules into one program */
+
+			for (module_t *module : gps.modules) {
+				/* get a list of all top-level decls */
+				std::set<std::string> bindings = get_top_level_decls(module->decls, module->type_decls, module->type_classes);
+				module_t *module_rebound = prefix(bindings, module);
+
+				/* now all locally referring vars are fully qualified */
+				for (decl_t *decl : module_rebound->decls) {
+					program_decls.push_back(decl);
+				}
+				for (type_class_t *type_class : module_rebound->type_classes) {
+					program_type_classes.push_back(type_class);
+				}
+			}
+
+			return std::make_shared<compilation_t>(
+					program_name,
+					new program_t(
+						program_decls,
+						program_type_classes,
+						new application_t(
+							new var_t(make_iid("main")),
+							new var_t(make_iid("unit")))),
+					gps.comments,
+					gps.link_ins);
+		} catch (user_error &e) {
+			print_exception(e);
+			return nullptr;
 		}
 	}
-	return working_key;
 }
--- src/compiler.h
+++ src/compiler.h
@@ -7,13 +7,28 @@
 #include "parse_state.h"
 #include "infer.h"
 
-struct compiler_t {
-	typedef std::vector<std::string> libs;
+struct compilation_t {
+	using ref = std::shared_ptr<compilation_t>;
+	compilation_t(
+			std::string program_name,
+			bitter::program_t *program,
+			std::vector<token_t> comments,
+			std::set<token_t> link_ins) :
+		program_name(program_name),
+		program(program),
+		comments(comments),
+		link_ins(link_ins)
+	{
+	}
 
-	compiler_t() = delete;
-	compiler_t(const compiler_t &) = delete;
-	compiler_t(std::string program_name);
-	~compiler_t();
+	std::string const program_name;
+	bitter::program_t * const program;
+	std::vector<token_t> const comments;
+	std::set<token_t> const link_ins;
+};
+
+namespace compiler {
+	typedef std::vector<std::string> libs;
 
 	void info(const char *format, ...);
 
@@ -21,28 +36,15 @@ struct compiler_t {
 	std::string dump_program_text(std::string module_name);
 
 	/* first step is to parse all modules */
-	bool parse_program();
-
-	bool build_type_check_and_code_gen();
+	compilation_t::ref parse_program(std::string program_name);
 
 	/* parse a single module */
 	bitter::module_t *parse_module(location_t location, std::string module_name);
-
-	std::string get_executable_filename() const;
-
-	bitter::program_t *program = nullptr;
-
-private:
-	std::string program_name;
-	std::vector<token_t> comments;
-	std::set<token_t> link_ins;
-
-	friend bool _check_compiler_error(compiler_t &compiler, int &skipped);
+	std::string resolve_module_filename(
+			location_t location,
+			std::string name,
+			std::string extension);
 };
 
 std::string strip_zion_extension(std::string module_name);
 const std::vector<std::string> &get_zion_paths();
-std::string resolve_module_filename(
-		location_t location,
-		std::string name,
-		std::string extension);
