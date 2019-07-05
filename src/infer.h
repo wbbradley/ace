5fa0d8dafab0e8b38ff10dacc59738999df38cd0
William Bradley <williambbradley@gmail.com>
2019-07-04 21:49:44 -0600

    add a multi-parameters type class test

--- src/infer.h
+++ src/infer.h
@@ -1,26 +1,11 @@
 #pragma once
-#include <tuple>
-#include <vector>
 
-#include "context.h"
+#include "ast.h"
+#include "class_predicate.h"
+#include "constraint.h"
 #include "env.h"
-#include "types.h"
 
-struct Constraint {
-  Constraint() = delete;
-  Constraint(types::Ref a, types::Ref b, Context &&context);
-
-  types::Ref a;
-  types::Ref b;
-  Context context;
-
-  void rebind(const types::Map &env);
-  std::string str() const;
-};
-
-typedef std::vector<Constraint> Constraints;
 types::Ref infer(const bitter::Expr *expr,
                  Env &env,
                  Constraints &constraints,
                  types::ClassPredicates &instance_requirements);
-std::string str(const Constraints &constraints);
